da9a0060e68bf6e9f044d217c4dd58eeab43192e
Pedro Geraghty Fleming <pedrogfleming@gmail.com>
2022-10-23 11:52:10 -0300

    changed the folder hierarchy

---
+++ RealEstateApp/RealEstateApp/src/main/java/com/realestateapp/ApartmentRaterTest.java
@@ -0,0 +1,16 @@
+package com.realestateapp;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ApartmentRaterTest {
+
+	@Test
+	void should_ReturnCorrectRating_When_CorrectApartment() {
+		Apartment ap = new Apartment(20 , 10000);
+		ApartmentRater apartRater = new ApartmentRater();
+	
+	}
+
+}
