a7541135aef7a5dffd7913873e684644bcdb57c4
Pedro Fleming <pedro.fleming@globallogic.com>
2022-10-23 17:34:07 -0300

    Spring book app added

---
+++ spring5webapp/src/main/java/guru/springframework/spring5webapp/domain/Publisher.java
@@ -0,0 +1,103 @@
+package guru.springframework.spring5webapp.domain;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+public class Publisher {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String name;
+    private String adressLine1;
+    private String city;
+    private String state;
+    private String zipCode;
+    @OneToMany
+    @JoinColumn(name = "publisher_id")
+    private Set<Book> books = new HashSet<>();
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAdressLine1() {
+        return adressLine1;
+    }
+
+    public void setAdressLine1(String adressLine1) {
+        this.adressLine1 = adressLine1;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public Set<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(Set<Book> books) {
+        this.books = books;
+    }
+
+    @Override
+    public String toString() {
+        return "Publisher{" +
+                "Id=" + id +
+                ", Name='" + name + '\'' +
+                ", AdressLine='" + adressLine1 + '\'' +
+                ", City='" + city + '\'' +
+                ", State='" + state + '\'' +
+                ", ZipCode='" + zipCode + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Publisher publisher = (Publisher) o;
+
+        return id == publisher.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return (int) (id ^ (id >>> 32));
+    }
+}
